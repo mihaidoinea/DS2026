83ee60a69e0fc43930cc26271fc4be831857f755
mdoinea <mihai.doinea@oracle.com>
2026-02-26 10:24:49 +0200

    [G1067] - Template

---
+++ G1067/Source.c
@@ -0,0 +1,78 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#pragma pack(1)
+typedef struct Student {
+	unsigned int regNo;
+	short int groupNo;
+	char* name;
+} Student, *PStudent;
+//typedef struct Student Student;
+#define LINE_BUFFER 256
+
+Student* createStudent(unsigned int, short int, const char*);
+void printStudent(Student*);
+
+int main()
+{
+	Student* stud1 = NULL;
+	PStudent stud2 = NULL;
+	printf("sizeof(Student)=%d\n", sizeof(Student));
+	FILE* fp = fopen("Data.txt", "r");
+	if (fp != NULL)
+	{
+		char line[LINE_BUFFER];
+		char* delimiter = ",";
+		//char delimiter[] = {',','\0'};
+		unsigned int regNo;
+		short int groupNo;
+		char* token = NULL;
+		char* context = NULL;
+		while (fgets(line, LINE_BUFFER, fp))
+		{
+			token = strtok_s(line, delimiter, &context);
+			regNo = atoi(token);
+			
+			token = strtok_s(NULL, delimiter, &context);
+			groupNo = atoi(token);
+
+			token = strtok_s(NULL, delimiter, &context);
+
+			Student* stud = createStudent(regNo, groupNo, token);
+			printStudent(stud);
+		}
+	}
+}
+
+void printStudent(Student* pStud)
+{
+	if(pStud != NULL)
+		printf("RegNo=%d, GroupNo=%d, Name=%s", 
+			pStud->regNo, 
+			pStud->groupNo, 
+			pStud->name);
+}
+
+Student* createStudent(unsigned int regNo, 
+	short int groupNo, 
+	const char* name)
+{
+	Student* pStud = (Student*)malloc(sizeof(Student));
+	if (pStud != NULL)
+	{
+		pStud->regNo = regNo;
+		pStud->groupNo = groupNo;
+		pStud->name = (char*)malloc(strlen(name) + 1);
+		if (pStud->name != NULL)
+		{
+			strcpy_s(pStud->name, strlen(name) + 1, name);
+		}
+		else
+		{
+			free(pStud);
+			pStud = NULL;
+		}
+	}
+	return pStud;
+}
