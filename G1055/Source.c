980befe3f34e6c44d962d49f03b8d9b348e7601b
mdoinea <mihai.doinea@oracle.com>
2026-03-12 09:07:25 +0200

    G1055 - S03

--- G1055/Source.c
+++ G1055/Source.c
@@ -15,9 +15,26 @@ typedef struct Student
 PStudent createStudent(unsigned int reg, unsigned short group, const char* name);
 void deleteStudent(Student* stud);
 void printStudent(Student* stud);
-
+void insertIntoArray(Student*** masiv, Student* stud, short* index);
+Student** insertIntoArrayReturn(Student** masiv, Student* stud, short* index);
 int main()
 {
+
+	//1. vector alocat static de elemente de tip Student
+	Student masiv1[6];
+	//2a. vector alocat dinamic de elemente de tip Student
+	//2b. pointer la un element de tip Student
+	Student* masiv2 = NULL;
+	//3a. vector alocat static de elemente de tip Student*
+	//3b. vector alocat static de vector de elemente de tip Student
+	Student* masiv3[6];
+	//4a.pointer la pointer la un element de tip Student
+	//4b.vector alocat dinamic de elemente de tip pointer la Student
+	//4c.matrice de elemente de tip Student
+	Student** masiv4 = NULL;
+
+	short index = 0;
+
 	Student stud = {.regNo=12300, .groupNo=1055, .name="Popescu Ioan"};
 	printf("sizeof(Student)=%d\n", sizeof(Student));
 
@@ -30,7 +47,7 @@ int main()
 		char* token = NULL, * context = NULL;
 		unsigned int reg;
 		unsigned short group;
-		char* buffer[LINE_SIZE];
+		char buffer[LINE_SIZE];
 		while (fgets(line, LINE_SIZE, pFile))
 		{
 			token = strtok_s(line, delimiter, &context);
@@ -45,12 +62,60 @@ int main()
 			//printf("Remaining string: %s\n", context);
 
 			Student* stud = createStudent(reg, group, buffer);
-			printStudent(stud);
+
+			//1.
+			masiv1[index] = *stud;
+			//masiv1[index].groupNo = group;
+			//masiv1[index].regNo = reg;
+			
+			//3.
+			masiv3[index] = stud;
+			//insertIntoArray(&masiv4, stud, &index);
+			masiv4 = insertIntoArrayReturn(masiv4, stud, &index);
+
+			printStudent(masiv4[index-1]);
+			deleteStudent(stud);
 
 		}
 	}
 	return 0;
 }
+Student** insertIntoArrayReturn(Student** masiv, Student* stud, short* index)
+{
+	//4.
+	Student** tmp = masiv;
+	masiv = (Student**)malloc(((*index) + 1) * sizeof(Student*));
+	if (masiv != NULL)
+	{
+		if (*index > 0)
+		{
+			for (register short i = 0; i < *index; i++)
+				masiv[i] = tmp[i];
+			free(tmp);
+		}
+		masiv[*index] = stud;
+	}
+	(*index)++;
+	return masiv;
+}
+void insertIntoArray(Student*** masiv, Student* stud, short* index)
+{
+	//4.
+	Student** tmp = *masiv;
+	*masiv = (Student**)malloc(((*index) + 1) * sizeof(Student*));
+	if (*masiv != NULL)
+	{
+		if (*index > 0)
+		{
+			for (register short i = 0; i < *index; i++)
+				(*masiv)[i] = tmp[i];
+			free(tmp);
+		}
+		(*masiv)[*index] = stud;
+	}
+	(*index)++;
+}
+
 PStudent createStudent(unsigned int reg, unsigned short group, const char* name)
 {
 	Student* stud = (Student*)malloc(sizeof(Student));
