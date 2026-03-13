a7751da7e72f9d0603bf8eb1c26d9403b9e8d86e
mdoinea <mihai.doinea@oracle.com>
2026-03-13 14:12:47 +0200

    G1055-S04

--- G1055/Source.c
+++ G1055/Source.c
@@ -12,12 +12,24 @@ typedef struct Student
 //typedef struct Student* PStudent;
 #define LINE_SIZE 256
 
-PStudent createStudent(unsigned int reg, unsigned short group, const char* name);
-void deleteStudent(Student* stud);
-void printStudent(Student* stud);
+typedef struct ListNode
+{
+	Student* data;
+	struct ListNode* next;
+}ListNode, *PListNode;
+
+PStudent createStudent(unsigned int, unsigned short, const char*);
+void deleteStudent(Student*);
+void printStudent(Student*);
+void insertIntoLinkedList(ListNode**, Student*);
+void printSinglyLinkedList(PListNode);
+PListNode insertIntoHeadLinkedList(PListNode, Student*);
+void deleteSinglyLinkedList(ListNode**);
 
 int main()
 {
+	ListNode* simplyLinkedList = NULL;
+
 	Student stud = {.regNo=12300, .groupNo=1055, .name="Popescu Ioan"};
 	printf("sizeof(Student)=%d\n", sizeof(Student));
 
@@ -46,10 +58,77 @@ int main()
 
 			Student* stud = createStudent(reg, group, buffer);
 
+			//	insertIntoEndLinkedList(&simplyLinkedList, stud);
+			simplyLinkedList = insertIntoHeadLinkedList(simplyLinkedList, stud);
+			printSinglyLinkedList(simplyLinkedList);
+			printf("\n-------------------------------\n");
+
 		}
+		deleteSinglyLinkedList(&simplyLinkedList);
+
+		printSinglyLinkedList(simplyLinkedList);
 	}
 	return 0;
 }
+
+void deleteSinglyLinkedList(ListNode** simplyLinkedList)
+{
+	while (*simplyLinkedList)
+	{
+		ListNode* tmp = *simplyLinkedList;
+		*simplyLinkedList = (*simplyLinkedList)->next;
+		free(tmp->data->name);
+		free(tmp->data);
+		free(tmp);
+	}
+}
+
+PListNode createNode(PStudent stud)
+{
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	if (node != NULL)
+	{
+		node->data = stud;
+		node->next = NULL;
+	}
+	return node;
+}
+PListNode insertIntoHeadLinkedList(PListNode headList, Student* pStud)
+{
+	ListNode* node = createNode(pStud);
+	if (node != NULL)
+	{
+		node->next = headList;
+		return node;
+	}
+	return headList;
+}
+
+void printSinglyLinkedList(PListNode headList)
+{
+	while (headList)
+	{
+		printStudent(headList->data);
+		headList = headList->next;
+	}
+}
+void insertIntoLinkedList(ListNode** headList, Student* pStud)
+{
+	ListNode* node = createNode(pStud);
+	if (node != NULL) 
+	{
+		if (*headList != NULL)
+		{
+			PListNode aux = *headList;
+			while (aux->next) // while(headList->next != NULL)
+				aux = aux->next;
+			aux->next = node;
+		}
+		else
+			*headList = node;
+	}
+}
+
 PStudent createStudent(unsigned int reg, unsigned short group, const char* name)
 {
 	Student* stud = (Student*)malloc(sizeof(Student));
