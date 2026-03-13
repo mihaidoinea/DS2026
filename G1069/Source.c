87127670cc9c9cbc6cff24a5d71048228babe7e0
mdoinea <mihai.doinea@oracle.com>
2026-03-13 09:42:54 +0200

    G1069-S04

--- G1069/Source.c
+++ G1069/Source.c
@@ -10,17 +10,31 @@ typedef struct Student
 } Student, *PStudent;
 //typedef struct Student Student;
 //typedef struct Student* PStudent;
+
+typedef struct ListNode
+{
+	Student* data;
+	struct ListNode* next;
+}ListNode, *PListNode;
+
+PStudent createStudent(unsigned int, unsigned short, const char*);
+void insertStudentIntoSinglyLinkedList(ListNode*, Student*);
+void deleteStudent(Student*);
+void printStudent(PStudent);
+
 #define LINE_SIZE 256
 int main()
 {
+	ListNode* singlyLinkedList = NULL;
+//	ListNode list;
 	Student stud = { .regNo = 13000,.groupNo = 1069,.name = "Popescu Ioan" };
 	printf("sizeof(Student)=%d\n", sizeof(Student));
 
 	FILE* pFile = fopen("Data.txt", "r");
 	if (pFile != NULL)
 	{
-		char* delimiter = ",";
-		char delimiter[] = { ',','\0' };
+		//char* delimiter = ",";
+		char delimiter[] = { ',','\n','\0' };
 		char line[LINE_SIZE];
 		char* token = NULL, *context = NULL;
 		unsigned int regNo; 
@@ -38,9 +52,23 @@ int main()
 			token = strtok_s(NULL, delimiter, &context);
 			Student* stud = createStudent(regNo, groupNo, token);
 
+			insertStudentIntoSinglyLinkedList(singlyLinkedList, stud);
 		}
 	}
 	return 0;
+}
+void insertStudentIntoSinglyLinkedList(ListNode* list, Student* stud)
+{
+
+}
+
+void printStudent(PStudent pStud)
+{
+	if (pStud != NULL)
+	{
+		printf("Student: %s, regNo: %d, groupNo: %d\n",
+			pStud->name, pStud->groupNo, pStud->regNo);
+	}
 }
 
 PStudent createStudent(unsigned int reg, unsigned short group,
