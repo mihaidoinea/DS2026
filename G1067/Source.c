23e1cf62843229e121d39f06bd67236ba1594d91
Mihai Doinea <mihai.doinea@oracle.com>
2026-03-23 14:22:31 +0200

    G1067-S05

--- G1067/Source.c
+++ G1067/Source.c
@@ -2,47 +2,138 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #pragma pack(1)
 typedef struct Student {
 	unsigned int regNo;
 	short int groupNo;
-	char* name; 
-} Student, *PStudent;
+	char* name;
+} Student, * PStudent;
+
+typedef struct StackNode
+{
+	Student* data;
+	struct StackNode* next;
+}StackNode, * PStackNode;
+
+typedef struct DoubleStack
+{
+	StackNode* head1;
+	StackNode* head2;
+}DoubleStack;
+
 //typedef struct Student Student;
 #define LINE_BUFFER 256
 
 Student* createStudent(unsigned int, short int, const char*);
 void printStudent(Student*);
 void deleteStudent(Student*);
+bool isEmpty(StackNode*);
+StackNode* push(StackNode*, Student*);
+Student* pop(StackNode**);
+Student* peek(StackNode*);
+
+bool isEmpty(StackNode* head)
+{
+	if (head != NULL)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+StackNode* push(StackNode* head, Student* stud)
+{
+	StackNode* newNode = (StackNode*)malloc(sizeof(StackNode));
+	newNode->data = stud;
+	newNode->next = NULL;
+	if (isEmpty(head) == false)
+	{
+		newNode->next = head;
+	}
+	return newNode;
+}
+
+Student* pop(StackNode** head) {
+	if (isEmpty(*head) == true)
+		return NULL;
+	else
+	{
+		// 1. return the useful info
+		// 2. delete the node
+		// 3. change the head
+
+		Student* stud = (*head)->data;
+		StackNode* tmp = *head;
+		(*head) = (*head)->next;
+		free(tmp);
+		return stud;
+	}
+}
+
+Student* peek(StackNode* head) {
+	if (isEmpty(head) == true)
+	{
+		return NULL;
+	}
+	else
+	{
+		return head->data;
+	}
+}
+
+void deleteStack(StackNode** stack);
 
 int main()
 {
+	PStackNode stack = NULL;
+	DoubleStack doubleStack = { .head1 = NULL, .head2 = NULL };
+
 	FILE* fp = fopen("Data.txt", "r");
 
 	if (fp != NULL)
 	{
 		char line[LINE_BUFFER];
 		//char* delimiter = ",";
-		char delimiter[] = {',','\n','\0'};
+		char delimiter[] = { ',','\n','\0' };
 		unsigned int regNo;
 		short int groupNo;
 		char* token = NULL;
 		char* context = NULL;
 		while (fgets(line, LINE_BUFFER, fp))
 		{
+
 			token = strtok_s(line, delimiter, &context);
 			regNo = atoi(token);
-			
+
 			token = strtok_s(NULL, delimiter, &context);
 			groupNo = atoi(token);
 
 			token = strtok_s(NULL, delimiter, &context);
 
 			Student* stud = createStudent(regNo, groupNo, token);
-	
+			stack = push(stack, stud);
+			stud = peek(stack);
+			printStudent(stud);
+
 		}
+		deleteStack(&stack);
 	}
 }
+
+void deleteStack(StackNode** stack) {
+	while (isEmpty(*stack) == false)
+	{
+		Student* stud = pop(stack);
+		printStudent(stud);
+		deleteStudent(stud);
+	}
+}
+
 void deleteStudent(Student* pStud)
 {
 	if (pStud != NULL)
@@ -54,15 +145,15 @@ void deleteStudent(Student* pStud)
 }
 void printStudent(Student* pStud)
 {
-	if(pStud != NULL)
-		printf("RegNo=%d, GroupNo=%d, Name=%s\n", 
-			pStud->regNo, 
-			pStud->groupNo, 
+	if (pStud != NULL)
+		printf("RegNo=%d, GroupNo=%d, Name=%s\n",
+			pStud->regNo,
+			pStud->groupNo,
 			pStud->name);
 }
 
-Student* createStudent(unsigned int regNo, 
-	short int groupNo, 
+Student* createStudent(unsigned int regNo,
+	short int groupNo,
 	const char* name)
 {
 	Student* pStud = (Student*)malloc(sizeof(Student));
