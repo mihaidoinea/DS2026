777aaa731527bcc2395cbb316c6b14c0582f869f
Mihai Doinea <mihai.doinea@oracle.com>
2026-03-30 21:48:03 +0300

    G1048_G06_Queue implementation

--- G1048/Source.c
+++ G1048/Source.c
@@ -2,23 +2,44 @@
 #include <string.h>
 #include <stdlib.h>
 //#pragma pack(1)
-
+#define LINE_SIZE 256
 typedef struct Student
 {
 	unsigned int regNo;
 	unsigned short group;
 	char* name;
-} Student, *PStudent;
+} Student, * PStudent;
+
+typedef struct QUEUENODE
+{
+	Student* data;
+	struct QUEUENODE* next;
+	struct QUEUENODE* prev;
+
+}QUEUENODE;
+
+//DUMMY IMPLEMENTATION
+typedef struct QUEUE
+{
+	struct QUEUENODE* head;
+	struct QUEUENODE* tail;
+}QUEUE;
+
+
 //typedef struct Student Student;
 //typedef struct Student* PStudent;
-#define LINE_SIZE 256
-
-PStudent createStudent(unsigned int, unsigned short,const char*);
+PStudent createStudent(unsigned int, unsigned short, const char*);
 void printStudent(Student*);
 void deleteStudent(Student*);
-
+QUEUE put(QUEUE, Student*);
+Student* get(QUEUE*);
+void deleteQueue(QUEUE*);
 int main()
 {
+	QUEUE queue = { .head = NULL, .tail = NULL };
+	//queue.head = NULL;
+	//queue.tail = NULL;
+
 	FILE* pFile = fopen("Data.txt", "r");
 	if (pFile != NULL)
 	{
@@ -42,11 +63,23 @@ int main()
 			token = strtok_s(NULL, delimiter, &context);
 			//printf("Remaining line: %s\n", context);
 			Student* pStud = createStudent(regNo, groupNo, token);
-			
+			queue = put(queue, pStud);
+			//pStud = get(&queue);
+			//printStudent(pStud);
 		}
+		deleteQueue(&queue);
 	}
-	
+
 	return 0;
+}
+
+void deleteQueue(QUEUE* queue) {
+	Student* stud = NULL;
+	while ((stud = get(queue)) != NULL)
+	{
+		printStudent(stud);
+		deleteStudent(stud);
+	}
 }
 
 void deleteStudent(Student* pStud)
@@ -70,8 +103,8 @@ void printStudent(Student* pStud)
 	}
 }
 
-PStudent createStudent(unsigned int regNo, 
-	unsigned short groupNo, 
+PStudent createStudent(unsigned int regNo,
+	unsigned short groupNo,
 	const char* name)
 {
 	Student* stud = (Student*)malloc(sizeof(Student));
@@ -79,10 +112,10 @@ PStudent createStudent(unsigned int regNo,
 	{
 		stud->regNo = regNo;
 		stud->group = groupNo;
-		stud->name = (char*)malloc(strlen(name)+1);
+		stud->name = (char*)malloc(strlen(name) + 1);
 		if (stud->name != NULL)
 		{
-			strcpy_s(stud->name, strlen(name)+1, name);
+			strcpy_s(stud->name, strlen(name) + 1, name);
 		}
 		else
 		{
@@ -91,4 +124,43 @@ PStudent createStudent(unsigned int regNo,
 		}
 	}
 	return stud;
+}
+
+QUEUE put(QUEUE queue, Student* pStud)
+{
+	QUEUENODE* nodNou = (QUEUENODE*)malloc(sizeof(QUEUENODE));
+	nodNou->data = pStud;
+	nodNou->next = nodNou->prev = NULL;
+
+	if (queue.tail == NULL && queue.head == NULL)
+	{
+		queue.tail = queue.head = nodNou;
+	}
+	else
+	{
+		nodNou->prev = queue.tail;
+		queue.tail->next = nodNou;
+		queue.tail = nodNou;
+	}
+
+	return queue;
+}
+
+
+Student* get(QUEUE* queue)
+{
+	Student* value = NULL;
+	if (queue->tail != NULL && queue->head != NULL) {
+		value = queue->head->data;
+		QUEUENODE* tmp = queue->head;
+		queue->head = tmp->next;
+		if (queue->head == NULL) {
+			queue->tail = NULL;
+		}
+		else {
+			queue->head->prev = NULL;
+		}
+		free(tmp);
+	}
+	return value;
 }
