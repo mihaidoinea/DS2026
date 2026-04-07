d889e6eb209337e55947f2fc31c580a080bca170
mdoinea <mihai.doinea@oracle.com>
2026-04-07 12:08:54 +0300

    G1055-S07

--- G1055/Source.c
+++ G1055/Source.c
@@ -7,19 +7,34 @@ typedef struct Student
 	unsigned int regNo;
 	unsigned short groupNo;
 	char* name;
-} Student, *PStudent;
+} Student, * PStudent;
+typedef struct DoubleLinkedList
+{
+	Student* info;
+	struct DoubleLinkedList* next, * prev;
+}DoubleLinkedList;
+typedef struct HashTable
+{
+	DoubleLinkedList** items;//vector alocat dinamic de liste
+	unsigned int size;
+}HashTable;
+
 //typedef struct Student Student;
 //typedef struct Student* PStudent;
 #define LINE_SIZE 256
+#define HT_InitialSize 3
 
 PStudent createStudent(unsigned int reg, unsigned short group, const char* name);
 void deleteStudent(Student* stud);
 void printStudent(Student* stud);
+HashTable putStudent(HashTable, Student*);
+void printHashTable(HashTable);
 
 int main()
 {
-	Student stud = {.regNo=12300, .groupNo=1055, .name="Popescu Ioan"};
-	printf("sizeof(Student)=%d\n", sizeof(Student));
+	//Student stud = {.regNo=12300, .groupNo=1055, .name="Popescu Ioan"};
+	//printf("sizeof(Student)=%d\n", sizeof(Student));
+	HashTable hashTable = { .items = NULL , .size = 0 };
 
 	FILE* pFile = fopen("Data.txt", "r");
 	if (pFile != NULL)
@@ -46,10 +61,80 @@ int main()
 
 			Student* stud = createStudent(reg, group, buffer);
 
+			hashTable = putStudent(hashTable, stud);
 		}
+
+		printHashTable(hashTable);
+
+		//deleteStudentByKey(hashTable, "Popescu Ion");
 	}
 	return 0;
 }
+
+
+DoubleLinkedList* insertLDIC(DoubleLinkedList* list, Student* stud)
+{
+	DoubleLinkedList* nod = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));
+	nod->prev = NULL;
+	nod->next = NULL;
+	nod->info = stud;
+	if (list == NULL) {
+		nod->next = nod;
+		nod->prev = nod;
+		/*list = nod;*/
+		return nod;
+	}
+	else {
+		nod->prev = list->prev;
+		nod->next = list;
+		list->prev = nod;
+		nod->prev->next = nod;
+	}
+	return list;
+
+}
+
+void printHashTable(HashTable hashTable)
+{
+	for (unsigned int i = 0; i < hashTable.size; i++)
+	{
+		printf("Bucket: %d\n", i);
+		DoubleLinkedList* iterator = hashTable.items[i];
+		do
+		{
+			printStudent(iterator->info);
+			iterator = iterator->next;
+		} while (iterator != hashTable.items[i]);
+	}
+}
+
+int fhash(char* name, int size)
+{
+	int m = 0;
+	for (unsigned int i = 0; i < strlen(name); i++)
+	{
+		m += name[i];
+	}
+	return m % size;
+}
+
+HashTable putStudent(HashTable hashTable, Student* stud)
+{
+	if (hashTable.items == NULL || hashTable.size == 0)
+	{
+		hashTable.items = (DoubleLinkedList**)malloc(HT_InitialSize * sizeof(DoubleLinkedList*));
+		hashTable.size = HT_InitialSize;
+		memset(hashTable.items, 0, HT_InitialSize * sizeof(DoubleLinkedList*));
+	}
+	int index = fhash(stud->name, hashTable.size);
+
+	//insertLDIC(&hashTable.items[index], stud );
+
+	hashTable.items[index] = insertLDIC(hashTable.items[index], stud);
+	return hashTable;
+
+}
+
 PStudent createStudent(unsigned int reg, unsigned short group, const char* name)
 {
 	Student* stud = (Student*)malloc(sizeof(Student));
