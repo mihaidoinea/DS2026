29a5dfc15997140d6dd248cb0ea750ebe7eedcf5
mdoinea <mihai.doinea@oracle.com>
2026-05-21 09:29:15 +0300

    G1067_S13

--- G1067/Source.c
+++ G1067/Source.c
@@ -1,48 +1,132 @@
-//#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#pragma pack(1)
-typedef struct Student {
-	unsigned int regNo;
-	short int groupNo;
-	char* name; 
-} Student, *PStudent;
-//typedef struct Student Student;
-#define LINE_BUFFER 256
+//#pragma pack(1)
 
-Student* createStudent(unsigned int, short int, const char*);
+typedef struct Student
+{
+	unsigned int regNo;
+	unsigned short group;
+	char* name;
+} Student, * PStudent;
+typedef struct Neighbour
+{
+	struct Vertex* reference;
+	struct Neighbour* next;
+}Neighbour;
+typedef struct Vertex
+{
+	Student* info;
+	struct Vertex* next;
+	Neighbour* neighbours;
+}Vertex;
+Vertex* createVertex(Student*);
+Vertex* addVertex(Vertex*, Student*);
+void addEdges(Vertex*, unsigned int, unsigned int);
+//typedef struct Student Student;
+//typedef struct Student* PStudent;
+#define LINE_SIZE 256
+
+PStudent createStudent(unsigned int, unsigned short, const char*);
 void printStudent(Student*);
 void deleteStudent(Student*);
 
 int main()
 {
-	FILE* fp = fopen("Data.txt", "r");
-
-	if (fp != NULL)
+	FILE* pFile = fopen("Data.txt", "r");
+	if (pFile != NULL)
 	{
-		char line[LINE_BUFFER];
+		char line[LINE_SIZE];
 		//char* delimiter = ",";
-		char delimiter[] = {',','\n','\0'};
-		unsigned int regNo;
-		short int groupNo;
+		char delimiter[] = { ',','\n','\0' };
 		char* token = NULL;
 		char* context = NULL;
-		while (fgets(line, LINE_BUFFER, fp))
+		unsigned int regNo;
+		unsigned short groupNo;
+		while (fgets(line, LINE_SIZE, pFile))
 		{
 			token = strtok_s(line, delimiter, &context);
 			regNo = atoi(token);
-			
+			//printf("Remaining line: %s\n", context);
+
 			token = strtok_s(NULL, delimiter, &context);
 			groupNo = atoi(token);
+			//printf("Remaining line: %s\n", context);
 
 			token = strtok_s(NULL, delimiter, &context);
+			//printf("Remaining line: %s\n", context);
+			Student* pStud = createStudent(regNo, groupNo, token);
 
-			Student* stud = createStudent(regNo, groupNo, token);
-	
+			graph = addVertex(graph, pStud);
+			noVertices++;
 		}
+
+		addEdges(graph, 8700, 4500);
+		addEdges(graph, 8700, 17000);
+		addEdges(graph, 3000, 17000);
+		addEdges(graph, 3000, 15000);
+		addEdges(graph, 3000, 13000);
+		addEdges(graph, 4500, 15000);
+		addEdges(graph, 17000, 13000);
+	}
+
+	return 0;
+}
+
+Vertex* findVertex(Vertex* list, unsigned int key)
+{
+	while (list && list->info->regNo != key)
+	{
+		list = list->next;
+	}
+	return list;
+}
+Neighbour* insertNeighbour(Neighbour* neighbours, Vertex* vertex)
+{
+	Neighbour* node = (Neighbour*)malloc(sizeof(Neighbour));
+	if (node != NULL)
+	{
+		node->reference = vertex;
+		node->next = neighbours;
+	}
+	return node;
+}
+void addEdges(Vertex* listOfVertices,
+	unsigned int src, unsigned int dst)
+{
+	Vertex* srcVertex = findVertex(listOfVertices, src);;
+	Vertex* dstVertex = findVertex(listOfVertices, dst);;
+
+	if (srcVertex != NULL && dstVertex != NULL)
+	{
+		srcVertex->neighbours = insertNeighbour(srcVertex->neighbours, dstVertex);
+		dstVertex->neighbours = insertNeighbour(dstVertex->neighbours, srcVertex);
 	}
 }
+
+Vertex* createVertex(Student* stud)
+{
+	Vertex* node = (Vertex*)malloc(sizeof(Vertex));
+	if (node != NULL)
+	{
+		node->info = stud;
+		node->neighbours = NULL;
+		node->next = NULL;
+	}
+	return node;
+}
+
+Vertex* addVertex(Vertex* listOfVertices, Student* stud)
+{
+	Vertex* vertex = createVertex(stud);
+	if (vertex != NULL)
+	{
+		vertex->next = listOfVertices;
+		listOfVertices = vertex;
+	}
+	return listOfVertices;
+}
+
 void deleteStudent(Student* pStud)
 {
 	if (pStud != NULL)
@@ -52,34 +136,37 @@ void deleteStudent(Student* pStud)
 		free(pStud);
 	}
 }
+
 void printStudent(Student* pStud)
 {
-	if(pStud != NULL)
-		printf("RegNo=%d, GroupNo=%d, Name=%s\n", 
-			pStud->regNo, 
-			pStud->groupNo, 
-			pStud->name);
-}
-
-Student* createStudent(unsigned int regNo, 
-	short int groupNo, 
-	const char* name)
-{
-	Student* pStud = (Student*)malloc(sizeof(Student));
 	if (pStud != NULL)
 	{
-		pStud->regNo = regNo;
-		pStud->groupNo = groupNo;
-		pStud->name = (char*)malloc(strlen(name) + 1);
-		if (pStud->name != NULL)
+		printf("Name: %s, regNo: %d, groupNo: %d\n",
+			pStud->name,
+			pStud->regNo,
+			pStud->group);
+	}
+}
+
+PStudent createStudent(unsigned int regNo,
+	unsigned short groupNo,
+	const char* name)
+{
+	Student* stud = (Student*)malloc(sizeof(Student));
+	if (stud != NULL)
+	{
+		stud->regNo = regNo;
+		stud->group = groupNo;
+		stud->name = (char*)malloc(strlen(name) + 1);
+		if (stud->name != NULL)
 		{
-			strcpy_s(pStud->name, strlen(name) + 1, name);
+			strcpy_s(stud->name, strlen(name) + 1, name);
 		}
 		else
 		{
-			free(pStud);
-			pStud = NULL;
+			free(stud);
+			stud = NULL;
 		}
 	}
-	return pStud;
+	return stud;
 }
