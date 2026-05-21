2fd81ba06e945c07f5f1563178d5f81bcb3304f0
mdoinea <mihai.doinea@oracle.com>
2026-05-21 07:41:37 +0300

    DS13 - Graphs

---
+++ DS13/Source.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//#pragma pack(1)
+
+typedef struct Student
+{
+	unsigned int regNo;
+	unsigned short group;
+	char* name;
+} Student, * PStudent;
+#define LINE_SIZE 256
+
+PStudent createStudent(unsigned int, unsigned short, const char*);
+void printStudent(Student*);
+void deleteStudent(Student*);
+
+int main()
+{
+	FILE* pFile = fopen("Data.txt", "r");
+	if (pFile != NULL)
+	{
+		char line[LINE_SIZE];
+		//char* delimiter = ",";
+		char delimiter[] = { ',','\n','\0' };
+		char* token = NULL;
+		char* context = NULL;
+		unsigned int regNo;
+		unsigned short groupNo;
+		while (fgets(line, LINE_SIZE, pFile))
+		{
+			token = strtok_s(line, delimiter, &context);
+			regNo = atoi(token);
+			//printf("Remaining line: %s\n", context);
+
+			token = strtok_s(NULL, delimiter, &context);
+			groupNo = atoi(token);
+			//printf("Remaining line: %s\n", context);
+
+			token = strtok_s(NULL, delimiter, &context);
+			//printf("Remaining line: %s\n", context);
+			Student* pStud = createStudent(regNo, groupNo, token);
+
+		}
+	}
+
+	return 0;
+}
+
+void deleteStudent(Student* pStud)
+{
+	if (pStud != NULL)
+	{
+		if (pStud->name != NULL)
+			free(pStud->name);
+		free(pStud);
+	}
+}
+
+void printStudent(Student* pStud)
+{
+	if (pStud != NULL)
+	{
+		printf("Name: %s, regNo: %d, groupNo: %d\n",
+			pStud->name,
+			pStud->regNo,
+			pStud->group);
+	}
+}
+
+PStudent createStudent(unsigned int regNo,
+	unsigned short groupNo,
+	const char* name)
+{
+	Student* stud = (Student*)malloc(sizeof(Student));
+	if (stud != NULL)
+	{
+		stud->regNo = regNo;
+		stud->group = groupNo;
+		stud->name = (char*)malloc(strlen(name) + 1);
+		if (stud->name != NULL)
+		{
+			strcpy_s(stud->name, strlen(name) + 1, name);
+		}
+		else
+		{
+			free(stud);
+			stud = NULL;
+		}
+	}
+	return stud;
+}
